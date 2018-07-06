71c51cce62e225325c1d7154216e7179cbd47ebb
Hannah Guo <hguo2102@gmail.com>
2018-07-06 12:21:23 -0400

    Loops for generating squares

--- src/index.js
+++ src/index.js
@@ -16,27 +16,24 @@ class Board extends React.Component {
     renderSquare(i) {
         return (<Square
             value={this.props.squares[i]}
+            key={i}
             onClick={() => this.props.onClick(i)} />);
     }
 
     render() {
         return (
             <div>
-                <div className="board-row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                </div>
+                {
+                    [...Array(3)].map((i, x) => {
+                        return <div className="board-row" key={x}>
+                            {
+                                [...Array(3)].map((j, y) => {
+                                        return this.renderSquare(x + (y * 3))
+                                })
+                            }
+                        </div>
+                    })
+                }
             </div>
         );
     }
@@ -78,8 +75,6 @@ class Game extends React.Component {
     }
 
     jumpTo(step) {
-        console.log(step + " " + this.state.history.length);
-        console.log(this.state.history)
         this.setState({
             // history: this.state.history.splice(step),
             // lastClickLocation: this.state.lastClickLocation.splice(step),
